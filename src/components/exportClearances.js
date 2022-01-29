e8111855f70251f1b9f449c038109f86897da53f
Anubhab Karki <bhubhik@gmail.com>
2022-01-29 15:22:57 +1100

    Export clearances

--- src/components/exportClearances.js
+++ src/components/exportClearances.js
@@ -1,10 +1,11 @@
 import classes from './clearances.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Export = () => {
   return (
     <div>
       <p className={classes.title}>Export Clearances</p>
-      <svg width='50' height='25' className={classes.line}>
+      <svg className={classes.line}>
         <line
           x1='50'
           y1='0'
@@ -15,26 +16,74 @@ const Export = () => {
         />
       </svg>
       <div className={classes.content}>
-        <h4>Documentation</h4>
         <p>
-          To assist in approving the clearance of your goods, that have already
-          arrived or are in transit to Australia, CCA would seek and require the
-          following documents, including:
+          In order to export articles from Australia you will require an
+          Australian Harmonised Export Commodity Classification ( AHECC ) which
+          is an eight digit code to classify goods
         </p>
+        <p>
+          CCA is customs approved to declare your goods attaining an Export
+          cleared status. As there are thousands of AHECC classifications it can
+          sometimes be confusing.
+        </p>
+        <p>
+          We can assist you to correctly defining your goods, AHECC and
+          clearance.
+        </p>
+        <h4>Documentation</h4>
+        <p>Following documents are crucial for us to clear your goods:</p>
         <p>
           <ul>
-            <li>
-              Your personal information (copy of your passport, drivers license)
-            </li>
-            <li>Authorisation to act on your behalf</li>
+            <li>Export invoice</li>
+            <li>Booking confirmation</li>
+            <li>Any permits and licences may be required</li>
           </ul>
         </p>
-        <div className={classes.cards}>
-          <div>
-            <h5>Documents for </h5>
-          </div>
-          <div></div>
-        </div>
+        <h4>Customs</h4>
+        <p>
+          Exportation of goods are controlled by government legislations and
+          policies.
+        </p>
+        <p>
+          All goods intended to be exported from Australia with a value of AUD
+          2000 or more must be declared to Customs on an export declaration
+          (EDN).
+        </p>
+        <p>
+          CCA through its authority with the Interactive Customs System can
+          facilitate this process on your behalf.{' '}
+        </p>
+        <p>
+          Goods which are conditionally prohibited from export may not be
+          exported unless all necessary export permits are obtained. Any goods
+          that require a permit for export must be reported on an export
+          declaration regardless of value.
+        </p>
+        <p>
+          CCA can assist with compliance in obtaining these lawful permits
+          allowing the successful exportation of your goods.
+        </p>
+        <h4>Quarantine</h4>
+        <p>
+          Following goods are regulated by the Department of Agriculture and may
+          generally require additional documentary to attain Export clearance.
+        </p>
+        <ul>
+          <li>Food</li>
+          <li>Live animals</li>
+          <li>Animal products</li>
+          <li>Fish</li>
+          <li>Aquatic products</li>
+          <li>Plants and grains</li>
+        </ul>
+        <p>
+          At CCA we can guide you through the process.
+          <NavLink to='/contact-us'>
+            <strong>Contact us.</strong>
+          </NavLink>
+        </p>
+        <br />
+        <br />
       </div>
     </div>
   );
