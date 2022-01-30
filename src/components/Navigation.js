0efe651b07fc5ec1cc07b533a42a8ad07c74f401
Anubhab Karki <bhubhik@gmail.com>
2022-01-30 11:39:58 +1100

    Desktop View Complete

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,9 @@ import { Link, NavLink } from 'react-router-dom';
 import logo from './images/logo2.png';
 
 const Nav = () => {
+  const toggleMobileMenu = (menu) => {
+    menu.classList.toggle('open');
+  };
   return (
     <div className='header'>
       <Link to='/'>
@@ -28,12 +31,40 @@ const Nav = () => {
           <NavLink activeClassName='active' to='/contact-us'>
             <li>Contact Us</li>
           </NavLink>
-
           <NavLink activeClassName='active' to='/faq'>
             <li>FAQ</li>
           </NavLink>
         </ul>
       </nav>
+      <div id='hamburger-icon' className='close' onclick={toggleMobileMenu}>
+        <div class='bar1'></div>
+        <div class='bar2'></div>
+        <div class='bar3'></div>
+        <ul className='mobile-menu'>
+          <NavLink exact activeClassName='active' to='/'>
+            <li>Home</li>
+          </NavLink>
+          <NavLink activeClassName='active' to='/clearances'>
+            <div className='dropdown'>
+              <li activeClassName='active'>Clearances &#9660;</li>
+              <div class='dropdown-content'>
+                <NavLink className='clearances' to='/import-clearances'>
+                  <li activeClassName='dropdown-active'>Import Clearances</li>
+                </NavLink>
+                <NavLink className='clearances' to='/export-clearances'>
+                  <li activeClassName='dropdown-active'>Export Clearances</li>
+                </NavLink>
+              </div>
+            </div>
+          </NavLink>
+          <NavLink activeClassName='active' to='/contact-us'>
+            <li>Contact Us</li>
+          </NavLink>
+          <NavLink activeClassName='active' to='/faq'>
+            <li>FAQ</li>
+          </NavLink>
+        </ul>
+      </div>
     </div>
   );
 };
